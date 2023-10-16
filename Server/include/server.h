7b787922df6f672253d197f1f7872936c06e798d
havebridge <meyl0r@yandex.com>
2023-10-16 20:51:29 +0300

    0.0.1

---
+++ Server/include/server.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "../../Core/Core.h"
+
+
+#define	MAX_NUM_CLIENTS 2
+
+namespace UDPChat
+{
+	class Server
+	{
+	private:
+		WSAData wsa;
+
+		SOCKET server_socket;
+		int port;
+		std::string ip;
+
+		struct sockaddr_in server_info;
+		int server_info_lenght;
+
+		char* recieved_message;
+		int recieved_message_size;
+
+		std::string send_message;
+		int send_message_size;
+
+		struct sockaddr_in first_client_info;
+		struct sockaddr_in second_client_info;
+		struct sockaddr_in in;
+
+		bool is_first_client_connected;
+		bool is_second_client_connected;
+
+		std::ofstream client_handler_file;
+
+	private:
+		void ClientsHandler();
+		void ProcessMessage();
+		bool ProcessFile(Instance::type client_handler);
+
+	public:
+		explicit Server(std::string ip, int port) noexcept;
+		Server(const Server&) = delete;
+		Server operator=(const Server&) = delete;
+		~Server();
+
+	public:
+		bool Init();
+
+		void Start();
+		void Stop();
+	};
+}
---
+++ Server/src/main.cpp
@@ -0,0 +1,21 @@
+#include "../include/server.h"
+
+int main(int argc, char** argv)
+{
+	(void)argc;
+	(void)argv;
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	UDPChat::Server server("127.0.0.1", 8888);
+
+	if (server.Init())
+	{
+		server.Start();
+	}
+
+	server.Stop();
+
+	return 0;
+}
