bcd8304808d010eaa42cc4b552a162a82f4e5c84
havebridge <meyl0r@yandex.com>
2023-10-18 23:42:32 +0300

    refactoring

--- Client/include/client.h
+++ Client/include/client.h
@@ -8,7 +8,7 @@
 #include <mutex>
 #include <condition_variable>
 
-#include "../../Core/Core.h"
+#include "Core.h"
 
 
 namespace UDPChat
--- Client/src/client.cpp
+++ Client/src/client.cpp
@@ -1,4 +1,4 @@
-#include "../include/client.h"
+#include "client.h"
 
 #pragma warning(disable: 4996)
 
@@ -58,8 +58,8 @@ namespace UDPChat
 
 	std::string Client::GetClientExternalIp() const
 	{
-		HINTERNET net = InternetOpen(L"IP retriever", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
-		HINTERNET conn = InternetOpenUrl(net, L"http://myexternalip.com/raw", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+		HINTERNET net = InternetOpenW(L"IP retriever", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+		HINTERNET conn = InternetOpenUrlW(net, L"http://myexternalip.com/raw", NULL, 0, INTERNET_FLAG_RELOAD, 0);
 
 		char buffer[4096];
 		DWORD read;
